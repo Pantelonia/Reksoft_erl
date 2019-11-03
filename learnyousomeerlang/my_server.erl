4395566f1f849dd6d0d7c8b8058ee632baa49786
Paul <pavel_syrovatsky@mail.ru>
2019-11-03 23:34:39 +0300

    add example from book

---
+++ learnyousomeerlang/my_server.erl
@@ -0,0 +1,19 @@
+-module(my_server).
+-compile([export_all]).
+
+call(Pid, Msg)->
+	Ref = erlang:monitor(process, Pid),
+	Pid ! {sync, self(), Ref, Msg},
+	receive 
+		{Ref, Reply}->
+			erlang:demonitor(Ref, [flush]),
+			Reply;
+		{'Down', Ref, process, Pid, Reason}->
+			erlang:error(Reason)
+	after 5000 ->
+		erlang:error(timeout)
+	end.
+
+cast(Pid, Msg)->
+	Pid ! {async, Msg},
+	ok.
