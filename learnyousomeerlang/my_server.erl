fb0eec7403187cef5ca52825fe4da6849fe55456
Paul <pavel_syrovatsky@mail.ru>
2019-11-04 23:58:11 +0300

    update my_server

--- learnyousomeerlang/my_server.erl
+++ learnyousomeerlang/my_server.erl
@@ -17,3 +17,17 @@ call(Pid, Msg)->
 cast(Pid, Msg)->
 	Pid ! {async, Msg},
 	ok.
+
+reply({Pid, Ref}, Reply)->
+		Pid ! {Ref, Reply}.
+
+init(Module, InitialState)->
+		loop(Module,Module:init(InitialState)).
+
+loop(Module, State) ->
+		receive
+			{async, Msg}->
+					loop(Module, Module: handle_cast(Msg, State));
+			{sync, Msg} ->
+					loop(Module, Module: handle_cast(Msg, {Pid,Ref}, State))
+		end.
